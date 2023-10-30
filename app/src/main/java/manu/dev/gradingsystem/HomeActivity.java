91ff318eb226b1ab4a5593ecf04f48536c13f68e
Getange <emmanuelgetange48@gmail.com>
2023-10-30 23:22:33 +0300

    ADD:GradingSystem fullcode

--- app/src/main/java/manu/dev/gradingsystem/HomeActivity.java
+++ app/src/main/java/manu/dev/gradingsystem/HomeActivity.java
@@ -52,66 +52,96 @@ public class HomeActivity extends AppCompatActivity {
                         // Implement your grading logic here based on the score.
                         // You can set the result in a TextView or display it in any way you prefer.
                         String grade;
-                        if (mathScore >= 90) {
+                        if (mathScore >= 80) {
                             grade = "A";
-                        } else if (mathScore >= 80) {
-                            grade = "B";
                         } else if (mathScore >= 70) {
-                            grade = "C";
+                            grade = "B+";
+                        } else if (mathScore >= 60){
+                            grade = "B";
+                        } else if (mathScore >= 50) {
+                            grade = "B-";
+                        } else if (mathScore >= 40) {
+                            grade = "D+";
+                        } else if (mathScore >= 30) {
+                            grade = "D-";
                         } else {
-                            grade = "F";
+                            grade = "E";
                         }
 
                         // Set the calculated grade as the text of the TextView
                         mathGradeTextView.setText(grade);
 
                         String grade2;
-                        if (englishScore >= 90) {
+                        if (englishScore >= 80) {
                             grade2 = "A";
-                        } else if (englishScore >= 80) {
-                            grade2 = "B";
                         } else if (englishScore >= 70) {
-                            grade2 = "C";
+                            grade2 = "B+";
+                        } else if (englishScore >= 60){
+                            grade2 = "B";
+                        } else if (englishScore >= 50) {
+                            grade2= "B-";
+                        } else if (englishScore >= 40) {
+                            grade2 = "D+";
+                        } else if (englishScore >= 30) {
+                            grade2 = "D-";
                         } else {
-                            grade2 = "F";
+                            grade2 = "E";
                         }
 
                         englishGradeTextView.setText(grade2);
 
                         String grade3;
-                        if (kiswahiliScore >= 90) {
+                        if (kiswahiliScore >= 80) {
                             grade3 = "A";
-                        } else if (kiswahiliScore >= 80) {
-                            grade3 = "B";
                         } else if (kiswahiliScore >= 70) {
-                            grade3 = "C";
+                            grade3= "B+";
+                        } else if (kiswahiliScore >= 60){
+                            grade3 = "B";
+                        } else if (kiswahiliScore >= 50) {
+                            grade3= "B-";
+                        } else if (kiswahiliScore >= 40) {
+                            grade3 = "D+";
+                        } else if (kiswahiliScore >= 30) {
+                            grade3 = "D-";
                         } else {
-                            grade3 = "F";
+                            grade3 = "E";
                         }
 
                         kiswahiliGradeTextView.setText(grade3);
 
                         String grade4;
-                        if (scienceScore >= 90) {
+                        if (scienceScore >= 80) {
                             grade4 = "A";
-                        } else if (scienceScore >= 80) {
+                        } else if (scienceScore  >= 70) {
+                            grade4= "B+";
+                        } else if (scienceScore >= 60){
                             grade4 = "B";
-                        } else if (scienceScore >= 70) {
-                            grade4 = "C";
+                        } else if (scienceScore  >= 50) {
+                            grade4= "B-";
+                        } else if (scienceScore  >= 40) {
+                            grade4 = "D+";
+                        } else if (scienceScore  >= 30) {
+                            grade4 = "D-";
                         } else {
-                            grade4 = "F";
+                            grade4 = "E";
                         }
 
                         scienceGradeTextView.setText(grade4);
                         String grade5;
-                        if (socialScore >= 90) {
+                        if (socialScore >= 80) {
                             grade5 = "A";
-                        } else if (socialScore >= 80) {
-                            grade5 = "B";
                         } else if (socialScore >= 70) {
-                            grade5 = "C";
+                            grade5 = "B+";
+                        } else if (socialScore >= 60){
+                            grade5 = "B";
+                        } else if (socialScore  >= 50) {
+                            grade5 = "B-";
+                        } else if (socialScore >= 40) {
+                            grade5 = "D+";
+                        } else if (socialScore >= 30) {
+                            grade5 = "D-";
                         } else {
-                            grade5 = "F";
+                            grade5 = "E";
                         }
 
                         socialGradeTextView.setText(grade5);
